fb3c4bc616a1ba983663be35ba521b4310a516c6
gabriel.rost <110986987+gabriel-rost@users.noreply.github.com>
2025-03-25 20:43:33 -0300

    cookies e sessoes
    aula_25-03-2025

---
+++ cookies-e-sessoes/cookie/retrieve_cookie.php
@@ -0,0 +1,9 @@
+<?php
+    // retrieve_cookie.php
+
+    echo ("Nome: " . $_COOKIE["nome"]);
+    echo ("<br>");
+    echo ("Idade: " . $_COOKIE["idade"]);
+    echo ("<br>");
+    echo ("Time do coração: " . $_COOKIE["time"]);
+?>
---
+++ cookies-e-sessoes/session/session_destroy.php
@@ -0,0 +1,9 @@
+<?php
+    // session_destroy.php
+
+    // iniciar a sessao
+    session_start();
+
+    // fechar a sessao
+    session_destroy();
+?>
