5f306f5b66373f3e292b10e8df95b4267563db0c
gabriel.rost <110986987+gabriel-rost@users.noreply.github.com>
2025-04-01 20:17:37 -0300

    Delete cookies-e-sessoes directory

--- cookies-e-sessoes/cookie/retrieve_cookie.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // retrieve_cookie.php
-
-    echo ("Nome: " . $_COOKIE["nome"]);
-    echo ("<br>");
-    echo ("Idade: " . $_COOKIE["idade"]);
-    echo ("<br>");
-    echo ("Time do coração: " . $_COOKIE["time"]);
-?>
--- cookies-e-sessoes/session/session_destroy.php
+++
@@ -1,9 +0,0 @@
-<?php
-    // session_destroy.php
-
-    // iniciar a sessao
-    session_start();
-
-    // fechar a sessao
-    session_destroy();
-?>
